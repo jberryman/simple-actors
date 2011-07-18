63b2cddd9ee9856a1f75ac33154eca7a5ec3a643
Brandon Simmons <brandon.m.simmons@gmail.com>
2011-07-17 23:26:19 -0400

    Updated Test module

--- Test.hs
+++ Test.hs
@@ -2,44 +2,41 @@ module Main
     where
 
 import Control.Concurrent.Actors
+import Control.Concurrent.Chan
 
-main = 
+main = do
+    main1
     main2
 
 ------------------------
 main2 = do
-    (b,str) <- newChanPair
-    forkActor_ $ simpleFork_ b
-    -- WORKS:
-    receive str >>= print
+    c <- newChan
+    forkActor_ $ simpleFork_ c
+    readChan c >>= print
+    getChanContents c >>=
+        mapM_ print . take 9
 
-    -- BROKEN:
-    receiveList str >>=
-        mapM_ print . take 100
-
-simpleFork_ :: Mailbox Int -> Actor_
-simpleFork_ b = Actor $ \_-> do
-    mapM (send b) [1..10]
+simpleFork_ :: Chan Int -> Actor_
+simpleFork_ c = Actor $ \_-> do
+    mapM (send c) [1..10]
     done
 
 ------------------------
--- BROKEN:
-
 main1 = spawnWriters 100
 
 spawnWriters n = do
-    (mb,ioStr) <- newChanPair
+    c <- newChan
     -- fork n actors. Each will write its number to the mailbox 'out'
-    mapM_ (forkActor_ . printerActor mb) [1..n]
+    mapM_ (forkActor_ . printerActor c) [1..n]
 
-    receiveList ioStr >>=
+    getChanContents c >>=
         putStrLn . unwords . map show . take (n*10) 
 
-printerActor :: Mailbox Int -> Int -> Actor_
+printerActor :: Chan Int -> Int -> Actor_
 printerActor b n = Actor $ \_ -> do
     send b n
     continue_ $ printerActor b n
-    
 ------------------------
 
-
+main3 = do
+    undefined
