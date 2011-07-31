6c323b2a0f11a3b9bec18873f5388bc88a4a1e9f
Brandon Simmons <brandon.m.simmons@gmail.com>
2011-07-31 13:32:11 -0400

    Start some real testing

--- Test.hs
+++ Test.hs
@@ -3,11 +3,59 @@ module Main
 
 import Control.Concurrent.Actors
 import Control.Concurrent.Chan
+import Control.Concurrent(forkIO,threadDelay)
 import Data.Cofunctor
 
 main = do
-    main1
-    main2
+    forkActorQueueTest
+
+------------------------
+-- informal test that forkLocking is working:
+forkActorQueueTest = do
+    output <- newChan
+    -- fork the actor to send numbers to whomever is listening to 's'. The sends
+    -- inside the forked Actor_ will block until 's' has an Actor
+    (b,s) <- newChanPair
+    forkActor_ $ senderTo 1000 b
+    forkActorUsing s $ sendInputTo output 200
+    -- these will block 
+    forkActorUsing s $ sendInputTo output 200
+    forkActorUsing s $ sendInputTo output 200
+    forkActorUsing s $ sendInputTo output 200
+    forkActorUsing s $ sendInputTo output 200
+     -- output should be in order because actor forks waited their turns above:
+    getChanContents output >>=
+        putStrLn . unwords . map show . take 1000
+    putStrLn "DONE!"
+
+sendInputTo :: Chan Int -> Int -> Actor Int
+sendInputTo c n = Actor $ \i -> do
+    send c i
+    if n == 1 
+        then done
+        else continue $ sendInputTo c (n-1)
+
+senderTo :: Int -> Mailbox Int -> Actor_
+senderTo n c = Actor $ \_-> do
+    send c n
+    if n == 1
+        then done
+        else continue_ $ senderTo (n-1) c
+
+------------------------
+sendSyncTest = undefined
+
+
+------------------------
+-- A kind of "living binary tree"
+
+binaryTree = do
+    output <- newChan
+    m <- forkActor $ treeNode
+    undefined
+
+
+treeNode = undefined
 
 ------------------------
 main2 = do
@@ -17,6 +65,7 @@ main2 = do
     getChanContents c >>=
         mapM_ print . take 9
 
+--  sends  10  messages to   the closed over chan.
 simpleFork_ :: Chan Int -> Actor_
 simpleFork_ c = Actor $ \_-> do
     mapM (send c) [1..10]
@@ -32,18 +81,9 @@ spawnWriters n = do
 
     getChanContents c >>=
         putStrLn . unwords . map show . take (n*10) 
-
+ 
+-- actor prints its number to the closed over chan:
 printerActor :: Chan Int -> Int -> Actor_
 printerActor b n = Actor $ \_ -> do
     send b n
     continue_ $ printerActor b n
-------------------------
-
--- A kind of "living binary tree"
-
-main3 = do
-    output <- newChan
-    m <- forkActor $ treeNode
-
-
-treeNode = undefined
