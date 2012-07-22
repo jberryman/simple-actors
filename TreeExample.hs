74355d90ac01b37e16c49a1fb84066682085bb72
Brandon Simmons <brandon.m.simmons@gmail.com>
2012-07-22 16:53:41 -0400

    expanded tree example

--- TreeExample.hs
+++ TreeExample.hs
@@ -5,6 +5,10 @@ import Control.Concurrent.Actors
 import Control.Applicative
 import Control.Concurrent.MVar
 
+import Control.Concurrent.Chan.Split
+import Control.Concurrent.Chan.Class
+import Control.Concurrent(forkIO)
+
 
 {- 
  - We build a sort of living binary tree or dynamic sorting network from Actors.
@@ -19,6 +23,11 @@ nil = Receive $ do
     (Query _ var) <- received 
     send var False -- signal Int is not present in tree
     return nil     -- await next message
+   
+   <|> do          -- else, StreamQuery received
+    (StreamQuery v var) <- received 
+    send var (v,False) 
+    return nil     
 
    <|> do          -- else, Insert received
     l <- spawn nil -- spawn child nodes
@@ -35,7 +44,8 @@ branch l r v = loop where
              GT -> send r m
              EQ -> case m of -- signal Int present in tree:
                         (Query _ var) -> send var True
-                        _             -> return ()
+                        (StreamQuery _ var) -> send var (v, True)
+                        _ -> return ()
         return loop
 
 
@@ -45,6 +55,9 @@ type Node = Mailbox Operation
 data Operation = Insert { val :: Int }
                | Query { val :: Int
                        , sigVar :: MVar Bool }
+               -- variant that sends query result out through a Chan:
+               | StreamQuery { val :: Int
+                             , sigChan :: InChan (Int, Bool) }
 
 insert :: Node -> Int -> IO ()
 insert t = send t . Insert
@@ -56,6 +69,12 @@ query t a = do
     send t (Query a v)
     takeMVar v
 
+-- fork a process that does a bunch of queries, streaming the results back to returned Chan:
+streamQueries :: Node -> [Int] -> IO (OutChan (Int,Bool))
+streamQueries t as = do
+    (i,o) <- newSplitChan
+    forkIO $ mapM_ (send t . flip StreamQuery i) as
+    return o
 
 ---- TEST CODE: ----
 
@@ -63,3 +82,10 @@ main = do
     t <- spawn nil
     mapM_ (insert t) [5,3,7,2,4,6,8]
     mapM (query t) [1,5,0,7] >>= print
+
+mainStream = do
+    t <- spawn nil
+    mapM_ (insert t) [5,3,7,2,4,6,8]
+    resps <- getChanContents =<< streamQueries t [1,5,0,7]
+    print $ take 4 resps 
+
